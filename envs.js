82a6d6d10a89f052810cd8759a5f79bded7cf7a7
Jaison James <jaimathewpaul@gmail.com>
2020-05-26 16:12:58 -0400

    sample

--- envs.js
+++ envs.js
@@ -1,4 +1,4 @@
-export const TEST_APP_ID = BUILD_APP_ID || undefined;
+export const TEST_APP_ID = '2650C2C2-1F72-4DC4-B88B-9FC374447F66' || undefined;
 export const USER_ID = undefined;
 export const ACCESS_TOKEN = undefined;
 export const IS_ACCESS_TOKEN_NEEDED = false;
